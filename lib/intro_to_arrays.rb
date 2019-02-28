7c69640a7fd6e90a4a04b799a7182d12642f37af
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-02-28 11:14:14 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,32 @@
+def instantiate_new_array
+  return @my_new_array = []
+end
+
+def array_with_two_elements
+  return @my_two_array = [ "red", "blue"]
+end
+
+def first_element(array)
+  return @taylor_swift[0]
+end
+
+def third_element(array)
+  return @taylor_swift[2]
+end
+
+def last_element(array)
+  return @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(array)
+  return @south_east_asia.first
+end
+
+def last_element_with_array_methods(array)
+  return @south_east_asia.last
+end
+
+def length_of_array(array)
+  return @programming_languages.length
+end
+
